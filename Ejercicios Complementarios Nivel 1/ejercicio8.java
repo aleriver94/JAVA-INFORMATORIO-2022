d8e0e7fc61749787c81dc944076cd44a0f92df77
Luis Alejandro Torres <89668563+aleriver94@users.noreply.github.com>
2022-06-12 20:53:34 -0300

    Ejercicios Nivel 1 terminados

--- Ejercicios Complementarios Nivel 1/ejercicio8.java
+++ Ejercicios Complementarios Nivel 1/ejercicio8.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class ejercicio8 {
-    
+    private static String bienvenida = "Bienvenido";
+    public static void main(String[] args){
+        System.out.println(bienvenida);
+        System.out.println("Por favor, a continuacion ingrese los datos requeridos.");
+        System.out.println("Ingrese su nombre y apellido: ");
+        Scanner scan = new Scanner(System.in);
+        String na = scan.nextLine();
+        System.out.println("Ingrese su edad: ");
+        String age = scan.nextLine();
+        System.out.println("Ingrese su Direccion: ");
+        String dir = scan.nextLine();
+        System.out.println("Ingrese su ciudad: ");
+        String city = scan.nextLine();
+        String resultado = String.format("%s - %s - %s - %s", city, dir, age, na);
+        System.out.println(resultado);
+        scan.close();
+    }
 }
--- Ejercicios Complementarios Nivel 1/ejercicio9.java
+++ Ejercicios Complementarios Nivel 1/ejercicio9.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class ejercicio9 {
-    
+    private static String bienvenida = "Bienvenido";
+    public static void main(String[] args){
+        System.out.println(bienvenida);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Escriba su texto: ");
+        String texto = scan.nextLine();
+        System.out.println("Por favor, escriba la letra a contar: ");
+        char letra = scan.next().charAt(0);
+        char[] a=texto.toCharArray();
+        int cont = 0;
+        for (char i : a){
+            if(i == letra){
+                cont++;
+            }
+        }
+        String resultado = String.format("La letra %s se repite: %d veces", letra, cont);
+        scan.close();
+        System.out.println(resultado);
+    }
 }
