4fd40ca36ab2e57d5ebad0fd3ae2cd3ff604e06a
Luis Alejandro Torres <89668563+aleriver94@users.noreply.github.com>
2022-06-13 20:50:37 -0300

    Ejercicios Terminados

--- Ejercicios Complementarios Nivel 2/Ejercicio6.java
+++ Ejercicios Complementarios Nivel 2/Ejercicio6.java
@@ -1,3 +1,37 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+class Empleado {
+    public String nombreApellido;
+    public int dni;
+    public int horasTrabajadas;
+    public int horasValor;
+    public Empleado(int dni, String nombreApellido, int horasTrabajadas, int horasValor){
+        this.nombreApellido = nombreApellido;
+        this.dni = dni;
+        this.horasTrabajadas = horasTrabajadas;
+        this.horasValor = horasValor;
+    }
+    public String toString(){
+        return String.valueOf(this.dni)+" - "+this.nombreApellido;
+    }
+}
+
 public class Ejercicio6 {
-    
+    private static String bienvenida = "Bienvenido";
+    public static void main(String[] args){
+        System.out.println(bienvenida);
+        Set<Empleado> empleados = new HashSet<Empleado>();
+        Map<Integer, Integer> sueldos = new HashMap<Integer, Integer>();
+        empleados.add(new Empleado(34562322, "Jose Nuñez", 8, 400));
+        empleados.add(new Empleado(27344914,"Luis Torres", 6, 500));
+        for (Empleado i : empleados){
+            sueldos.put(i.dni, (i.horasTrabajadas*i.horasValor));
+            System.out.println("Sueldo para "+i.dni+" : "+sueldos.get(i.dni));
+        }
+
+    }
 }
--- Ejercicios Complementarios Nivel 2/Ejercicio7.java
+++ Ejercicios Complementarios Nivel 2/Ejercicio7.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+
 public class Ejercicio7 {
-    
+    public static ArrayList<String> fizzBuzz(int a, int b){
+        String palabra="";
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i=a; i<b-1; i++){
+            palabra = String.valueOf(i);
+            if(i%2==0){
+                palabra = "\"Fizz\"";
+            }
+            else{
+                if (i%3==0){
+                    palabra="\"Buzz\"";
+                }
+                else {
+                    if (i%2==0 && i%3==0){
+                        palabra=" \"FizzBuzz \"";
+                    }
+                    else{
+                        palabra="\""+String.valueOf(i)+"\"";
+                    }
+                }
+            }
+            list.add(palabra);
+        }
+        return list;
+    }
+    public static void main(String[] args){
+        ArrayList<String> cadena = fizzBuzz(1, 8);
+        System.out.print(cadena);
+    }
 }
