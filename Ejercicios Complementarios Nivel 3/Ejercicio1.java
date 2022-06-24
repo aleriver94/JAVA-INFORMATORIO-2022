b6d6a60f3d24c6f3baf3d5483050ea626467be33
Luis Alejandro Torres <89668563+aleriver94@users.noreply.github.com>
2022-06-24 13:31:17 -0300

    Update Ejercicio1.java

--- Ejercicios Complementarios Nivel 3/Ejercicio1.java
+++ Ejercicios Complementarios Nivel 3/Ejercicio1.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Ejercicio1 {
-    
+    private static String bienvenida = "Bienvenido, aqui filtraremos un ArrayList sacandole los null y los espacios en blanco.";
+    public static void main(String[] args) {
+        System.out.println(bienvenida);
+        List<String> palabras = new ArrayList<>();
+        palabras.add("Hola");
+        palabras.add(null);
+        palabras.add("Informatorio");
+        palabras.add("");
+        System.out.println("Lista sin filtrar: "+palabras);
+        filtrar(palabras);   
+    }
+    public static void filtrar (List<String> a){
+        List<String> listaFiltrada = a.stream().filter(p -> p!=null && p!="").collect(Collectors.toList());
+        System.out.println("Lista filtrada: "+listaFiltrada);
+    }
 }
+
